d90c73ac29bd3ba8532ea4e35675fa0c3991fb05
chmac <chmac@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-10-19 17:29:49 +0000

    Version 0.4.1 with additional error output
    git-svn-id: http://plugins.svn.wordpress.org/wp-mail-smtp/trunk@22501 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://www.callum-macdonald.com/code/donate/
 Tags: mail, smtp, wp_mail, mailer, phpmailer
 Requires at least: 2.0
 Tested up to: 2.3
-Stable tag: 0.4
+Stable tag: 0.4.1
 
 Reconfigures the wp_mail() function to use SMTP instead of mail() and creates an options page to manage host, username, password, etc.
 
--- wp_mail_smtp.php
+++ wp_mail_smtp.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WP-Mail-SMTP
-Version: 0.4
+Version: 0.4.1
 Plugin URI: http://www.callum-macdonald.com/code/wp-mail-smtp/
 Description: Reconfigures the wp_mail() function to use SMTP instead of mail() and creates an options page to manage host, username, password, etc.
 Author: Callum Macdonald
@@ -22,6 +22,7 @@ Author URI: http://www.callum-macdonald.com/
  * 
  * CHANGELOG
  * 
+ * 0.4.1 - Added $phpmailer->ErroInfo to the test mail output
  * 0.4 - Added the test email feature and cleaned up some other bits and pieces
  * 0.3.2 - Changed to use register_activation_hook for greater compatability
  * 0.3.1 - Added readme for WP-Plugins.org compatability
@@ -120,6 +121,8 @@ if (!function_exists('wp_mail_smtp_options_page')) {
 <div id="message" class="updated fade"><p><strong><?php _e('Test Message Sent'); ?></strong></p>
 <p><?php _e('The result was:'); ?></p>
 <pre><?php var_dump($result); ?></pre>
+<p><?php _e('The last error message (if any) was:'); ?></p>
+<pre><?php print($phpmailer->ErrorInfo); ?></pre>
 </div>
 			<?php
 			

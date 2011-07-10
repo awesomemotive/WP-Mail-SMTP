5bb2cd23f4ad726a4a1e9894483bafba2a26f4c0
chmac <chmac@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-10 14:03:20 +0000

    Forgot to bump version no in the php file. DOH.
    git-svn-id: http://plugins.svn.wordpress.org/wp-mail-smtp/trunk@407868 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp_mail_smtp.php
+++ wp_mail_smtp.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WP-Mail-SMTP
-Version: 0.9.0
+Version: 0.9.1
 Plugin URI: http://www.callum-macdonald.com/code/wp-mail-smtp/
 Description: Reconfigures the wp_mail() function to use SMTP instead of mail() and creates an options page to manage the settings.
 Author: Callum Macdonald

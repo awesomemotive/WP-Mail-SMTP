9fe8a9f373b22f81244e1f19eb3e4baeac337fc5
chmac <chmac@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-14 12:42:49 +0000

    Corrected apply_filters from do_action, rookie mistake.
    git-svn-id: http://plugins.svn.wordpress.org/wp-mail-smtp/trunk@544063 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp_mail_smtp.php
+++ wp_mail_smtp.php
@@ -106,7 +106,7 @@ function phpmailer_init_smtp($phpmailer) {
 		}
 		
 		// If you're using contstants, set any custom options here
-		$phpmailer = do_action('wp_mail_smtp_custom_options', $phpmailer);
+		$phpmailer = apply_filters('wp_mail_smtp_custom_options', &$phpmailer);
 		
 	}
 	else {
@@ -146,7 +146,7 @@ function phpmailer_init_smtp($phpmailer) {
 		
 		// You can add your own options here, see the phpmailer documentation for more info:
 		// http://phpmailer.sourceforge.net/docs/
-		$phpmailer = do_action('wp_mail_smtp_custom_options', $phpmailer);
+		$phpmailer = apply_filters('wp_mail_smtp_custom_options', &$phpmailer);
 		
 		
 		// STOP adding options here.

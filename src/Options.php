8c658b5a6aa7ba5603007cdd1745707ee17631e7
Slava Abakumov <slava.abakumov@gmail.com>
2017-10-12 21:04:29 +0300

    see #13 - Options class to get/set data.

--- src/Options.php
+++ src/Options.php
@@ -7,10 +7,50 @@ namespace WPMailSMTP;
  */
 class Options {
 
-	public static function get( ) {
+	/**
+	 * @var string
+	 */
+	const META_KEY = 'wp_mail_smtp';
+
+	/**
+	 * Get a value by a group and a key or values by group only:
+	 *
+	 * Options::get()               - will return all options.
+	 * Options::get('smtp')         - will return only SMTP options.
+	 * Options::get('smtp', 'host') - will return only SMTP 'host' option.
+	 *
+	 * @param string $group
+	 * @param string $key
+	 *
+	 * @return array|string
+	 */
+	public static function get( $group = '', $key = '' ) {
+
+		// Just to feel safe.
+		$group = sanitize_key( $group );
+		$key   = sanitize_key( $key );
+
+		$options = get_option( self::META_KEY, array() );
+
+		// Get the options group.
+		if ( array_key_exists( $group, $options ) ) {
+			// Get the options key of a group.
+			if ( array_key_exists( $key, $options[ $group ] ) ) {
+				return $options[ $group ][ $key ];
+			}
+
+			return $options[ $group ];
+		}
+
+		return $options;
 	}
 
-	public static function set() {
+	/**
+	 * Set plugin options, all at once.
+	 *
+	 * @param array $options
+	 */
+	public static function set( $options ) {
+		update_option( self::META_KEY, $options );
 	}
-
 }

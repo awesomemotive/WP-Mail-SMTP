5074a76c141570a82342cef8cd7029fdf6d1cbfa
Slava Abakumov <slava.abakumov@gmail.com>
2017-11-29 18:33:16 +0200

    switch to own autoloader, instead of composer.
    This is a preparation for #46 in core.

--- wp-mail-smtp.php
+++ wp-mail-smtp.php
@@ -5,6 +5,38 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
+ * Autoloader. Inspired by PSR-4 examples:
+ *
+ * @link https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
+ *
+ * @param string $class The fully-qualified class name.
+ */
+spl_autoload_register( function ( $class ) {
+
+	list( $plugin_space ) = explode( '\\', $class );
+	if ( $plugin_space !== 'WPMailSMTP' ) {
+		return;
+	}
+
+	$plugin_folder = 'wp-mail-smtp';
+
+	// Default directory for all code is plugin's /src/.
+	$base_dir = WP_PLUGIN_DIR . '/' . $plugin_folder . '/src/';
+
+	// Get the relative class name.
+	$relative_class = substr( $class, strlen( $plugin_space ) + 1 );
+
+	// Prepare a path to a file.
+	$file = wp_normalize_path( $base_dir . $relative_class . '.php' );
+
+	// If the file exists, require it.
+	if ( is_readable( $file ) ) {
+		/** @noinspection PhpIncludeInspection */
+		require_once $file;
+	}
+} );
+
+/**
  * Global function-holder. Works similar to a singleton's instance().
  *
  * @since 1.0.0
@@ -16,8 +48,6 @@ function wp_mail_smtp() {
 	 * @var \WPMailSMTP\Core
 	 */
 	static $core;
-
-	require __DIR__ . '/vendor/autoload.php';
 
 	if ( ! isset( $core ) ) {
 		$core = new \WPMailSMTP\Core();

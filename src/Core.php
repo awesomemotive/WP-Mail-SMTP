23204aa73899730aee2b149799ab0b24a1603ed6
Slava Abakumov <slava.abakumov@gmail.com>
2017-12-04 20:38:17 +0200

    fix the include path for MacOS (perhaps Linux too).

--- src/Core.php
+++ src/Core.php
@@ -29,8 +29,8 @@ class Core {
 	 */
 	public function __construct() {
 
-		$this->plugin_url  = trim( plugin_dir_url( __DIR__ ), '/\\' );
-		$this->plugin_path = trim( plugin_dir_path( __DIR__ ), '/\\' );
+		$this->plugin_url  = rtrim( plugin_dir_url( __DIR__ ), '/\\' );
+		$this->plugin_path = rtrim( plugin_dir_path( __DIR__ ), '/\\' );
 
 		$this->hooks();
 	}

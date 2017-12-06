2fd5e4a21058e4c9692db22ee286a4673247d3a6
Jared Atchison <jared@jaredatchison.com>
2017-12-05 21:09:56 -0600

    SendGrid options style adjustments

--- src/Providers/Sendgrid/Options.php
+++ src/Providers/Sendgrid/Options.php
@@ -33,44 +33,38 @@ class Options extends OptionAbstract {
 	public function display_options() {
 		?>
 
-		<table class="form-table">
-
-			<!-- API Key -->
-			<tr>
-				<th scope="row">
-					<label for="wp-mail-smtp-setting-<?php echo esc_attr( $this->get_slug() ); ?>-api-key"><?php esc_html_e( 'API Key', 'wp-mail-smtp' ); ?></label>
-				</th>
-				<td>
-					<input name="wp-mail-smtp[<?php echo esc_attr( $this->get_slug() ); ?>][api_key]" type="text"
-						value="<?php echo esc_attr( $this->options->get( $this->get_slug(), 'api_key' ) ); ?>"
-						<?php echo $this->options->is_const_defined( $this->get_slug(), 'api_key' ) ? 'disabled' : ''; ?>
-						id="wp-mail-smtp-setting-<?php echo esc_attr( $this->get_slug() ); ?>-api-key" class="regular-text" spellcheck="false"
-					/>
-
-					<p class="description">
-						<?php
-						printf(
-							/* translators: %s - API key link. */
-							esc_html__( 'Follow this link to get an API Key from Sendgrid: %s.', 'wp-mail-smtp' ),
-							'<a href="https://app.sendgrid.com/settings/api_keys" target="_blank">' .
-							esc_html__( 'Create API Key', 'wp-mail-smtp' ) .
-							'</a>'
-						);
-						?>
-					</p>
-					<p class="description">
-						<?php
-						printf(
-							/* translators: %s - Sendgrid access level. */
-							esc_html__( 'To send emails you will need only a %s access level for this API key.', 'wp-mail-smtp' ),
-							'<code>Mail Send</code>'
-						);
-						?>
-					</p>
-				</td>
-			</tr>
-
-		</table>
+		<!-- API Key -->
+		<div id="wp-mail-smtp-setting-row-<?php echo esc_attr( $this->get_slug() ); ?>-apli_key" class="wp-mail-smtp-setting-row wp-mail-smtp-setting-row-text wp-mail-smtp-clear">
+			<span class="wp-mail-smtp-setting-label">
+				<label for="wp-mail-smtp-setting-<?php echo esc_attr( $this->get_slug() ); ?>-apli_key"><?php esc_html_e( 'API Key', 'wp-mail-smtp' ); ?></label>
+			</span>
+			<span class="wp-mail-smtp-setting-field">
+				<input name="wp-mail-smtp[<?php echo esc_attr( $this->get_slug() ); ?>][apli_key]" type="text"
+					value="<?php echo esc_attr( $this->options->get( $this->get_slug(), 'apli_key' ) ); ?>"
+					<?php echo $this->options->is_const_defined( $this->get_slug(), 'apli_key' ) ? 'disabled' : ''; ?>
+					id="wp-mail-smtp-setting-<?php echo esc_attr( $this->get_slug() ); ?>-apli_key" spellcheck="false"
+				/>
+				<p class="desc">
+					<?php
+					printf(
+						/* translators: %s - API key link. */
+						esc_html__( 'Follow this link to get an API Key from Sendgrid: %s.', 'wp-mail-smtp' ),
+						'<a href="https://app.sendgrid.com/settings/api_keys" target="_blank">' .
+						esc_html__( 'Create API Key', 'wp-mail-smtp' ) .
+						'</a>'
+					);
+					?>
+					<br/>
+					<?php
+					printf(
+						/* translators: %s - Sendgrid access level. */
+						esc_html__( 'To send emails you will need only a %s access level for this API key.', 'wp-mail-smtp' ),
+						'<code>Mail Send</code>'
+					);
+					?>
+				</p>
+			</span>
+		</div>
 
 		<?php
 	}

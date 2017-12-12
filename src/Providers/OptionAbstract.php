748d6de3bdcf245cb8908ba347a09c07d37a2c06
Jared Atchison <jared@jaredatchison.com>
2017-12-12 16:27:16 -0600

    SMTPAutoTLS setting markup and JS, see #70

--- src/Providers/OptionAbstract.php
+++ src/Providers/OptionAbstract.php
@@ -188,6 +188,27 @@ abstract class OptionAbstract implements OptionInterface {
 			</div>
 		</div>
 
+		<!-- PHPMailer Disable SMTPAutoTLS -->
+		<div id="wp-mail-smtp-setting-row-<?php echo esc_attr( $this->get_slug() ); ?>-disable_autotls" class="wp-mail-smtp-setting-row wp-mail-smtp-setting-row-checkbox-toggle wp-mail-smtp-clear <?php echo $this->options->is_const_defined( $this->get_slug(), 'encryption' ) || 'tls' === $this->options->get( $this->get_slug(), 'encryption' ) ? 'inactive' : ''; ?>">
+			<div class="wp-mail-smtp-setting-label">
+				<label for="wp-mail-smtp-setting-<?php echo esc_attr( $this->get_slug() ); ?>-disable_autotls"><?php esc_html_e( 'Disable Auto TLS', 'wp-mail-smtp' ); ?></label>
+			</div>
+			<div class="wp-mail-smtp-setting-field">
+				<label for="wp-mail-smtp-setting-<?php echo esc_attr( $this->get_slug() ); ?>-disable_autotls">
+					<input type="checkbox" id="wp-mail-smtp-setting-<?php echo esc_attr( $this->get_slug() ); ?>-disable_autotls"
+						name="wp-mail-smtp[<?php echo esc_attr( $this->get_slug() ); ?>][disable_autotls]" value="yes"
+						<?php echo $this->options->is_const_defined( $this->get_slug(), 'disable_autotls' ) ? 'disabled' : ''; ?>
+						<?php checked( true, $this->options->get( $this->get_slug(), 'disable_autotls' ) ); ?>
+					/>
+					<span class="wp-mail-smtp-setting-toggle-switch"></span>
+					<span class="wp-mail-smtp-setting-toggle-checked-label"><?php esc_html_e( 'On', 'wp-mail-smtp' ); ?></span>
+					<span class="wp-mail-smtp-setting-toggle-unchecked-label"><?php esc_html_e( 'Off', 'wp-mail-smtp' ); ?></span>
+				</label>
+				<p class="desc">
+					<?php esc_html_e( 'By default TLS encryption is automatically used if the server supports it, which is recommended. In some cases, due to server misconfigurations, this can cause issues and may need to be disabled.', 'wp-mail-smtp' ); ?>
+				</p>
+			</div>
+		</div>
 
 		<!-- SMTP Authentication -->
 		<div id="wp-mail-smtp-setting-row-<?php echo esc_attr( $this->get_slug() ); ?>-auth" class="wp-mail-smtp-setting-row wp-mail-smtp-setting-row-checkbox-toggle wp-mail-smtp-clear">
